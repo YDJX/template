af9d520f6244b4223149745f4c44653aab0861ba
YDJX <f258fengye@163.com>
2014-07-24 23:03:58 +0800

    add

---
+++ src/main/java/org/daemon/belief/mythink/pojo/Userinfo.java
@@ -0,0 +1,73 @@
+package org.daemon.belief.mythink.pojo;
+
+
+public class Userinfo {
+
+	private int userId;
+	private String userName;
+	private String userPwd;
+	private String idCardNo;
+	private java.sql.Date birthDate;
+	private int age;
+	
+	private String degree; //学位
+	private String education; //学历
+	private String experience; //工作经验
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPwd() {
+		return userPwd;
+	}
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	public String getIdCardNo() {
+		return idCardNo;
+	}
+	public void setIdCardNo(String idCardNo) {
+		this.idCardNo = idCardNo;
+	}
+	public java.sql.Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(java.sql.Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getDegree() {
+		return degree;
+	}
+	public void setDegree(String degree) {
+		this.degree = degree;
+	}
+	public String getEducation() {
+		return education;
+	}
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public String getExperience() {
+		return experience;
+	}
+	public void setExperience(String experience) {
+		this.experience = experience;
+	}
+	
+	
+}
